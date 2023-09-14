3fbaa08e2466357e843640b184c64696ce6dc5e2
juansechaux <juansechaux@hotmail.com>
2023-09-14 09:39:47 -0700

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -30,6 +30,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 		return (1);
 	else
